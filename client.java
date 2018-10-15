51a279c56eafbbb484601760fb497977346cad29
DitteFabrin <ditte.brodersen@live.dk>
2018-10-15 11:19:24 +0200

    Revert "Update client.java"
    This reverts commit fac5b73071e3e8829610cde3fbfcf3c351ee2f09.

--- client.java
+++ client.java
@@ -20,41 +20,21 @@ public class client {
 				DataInputStream fromServer = null;
 				
 				
-				// Create a socket to connect to the server 
-				Socket serverSocket = new Socket("localhost", 2000); // This shouldn't be local host but  the serve machine’s host name or IP address
-				System.out.println("Client connected ");
-				
-				// Create an input stream to receive data from the server
-				fromServer = new DataInputStream(serverSocket.getInputStream());
-				
-				// Create an output stream to send data to the server
-				toServer = new DataOutputStream(serverSocket.getOutputStream());
-				
-				
 				
 				// Enter if you want to start the game
-				System.out.print("Do you want to start a game of rock, paper scissors? Type yes or no: ");
-				String wantToStartGame = input.next();
-				
-				
-				// Send to server
-				toServer.writeChars(wantToStartGame);
-				//toServer.flush();
-				
-				
-				
-				// If user types yes it continues if no it stops 
-				String answerStartGame = input.next();
+				System.out.print("Do you want to start a game of rock, paper scissors?: ");
+				String wanttostartgame = inputStartGame.toString();
+				String answerStartGame = inputStartGame.next();
 				if ( answerStartGame.equalsIgnoreCase("no")) {
 					keepRunning = false;
 				}
 				else {
-					// Pick rock, paper, scissors 
-					System.out.print("Pick! Rock = 1, paper = 2 or scissors = 3:");
-					
+					continue;
 				}
-			input.close();
+				inputStartGame.close();
+			
 			}
+			
 		catch(IOException e) {
 			
 		}
